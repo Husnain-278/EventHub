7828861a30d76b38bbaeff6be4dd3887cbbfc39d
husnain-278 <husnain327878@gmail.com>
2026-01-21 09:09:41 +0500

    initial setup

--- EventHub/settings.py
+++ EventHub/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'EventHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eventhub_dev',
+        'USER': 'postgres',
+        'PASSWORD': 'husnain3278@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
